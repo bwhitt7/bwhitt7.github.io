0401b3d0346217358c708ed31d6c3e41a8ab8d28
bwhitt7 <bwhitt7@students.towson.edu>
2022-10-24 19:44:08 -0400

    apod stuff

--- apod.js
+++ apod.js
@@ -1,16 +1,19 @@
-var req = new XMLHttpRequest();
-var url = "https://api.nasa.gov/planetary/apod?api_key=";
-var api_key = "xEJ0WheX967HwigGbbLqsPjW6gRoZhfoGl4sEhNV";
+window.onload = function(){
+  var req = new XMLHttpRequest();
+  var url = "https://api.nasa.gov/planetary/apod?api_key=";
+  var api_key = "xEJ0WheX967HwigGbbLqsPjW6gRoZhfoGl4sEhNV";
 
-req.open("GET", url + api_key);
-req.send();
+  req.open("GET", url + api_key);
+  req.send();
 
-req.addEventListener("load", function(){
-	if(req.status == 200 && req.readyState == 4){
-  	var response = JSON.parse(req.responseText);
-    document.getElementById("title").textContent = response.title;
-    document.getElementById("date").textContent = response.date;
-    document.getElementById("pic").src = response.hdurl;
-    document.getElementById("explanation").textContent = response.explanation;
-  }
-})
+  req.addEventListener("load", function(){
+    if(req.status == 200 && req.readyState == 4){
+      var response = JSON.parse(req.responseText);
+      document.getElementById("title").textContent = response.title;
+      document.getElementById("date").textContent = response.date;
+      document.getElementById("pic").src = response.hdurl;
+      document.getElementById("explanation").textContent = response.explanation;
+      document.body.style.backgroundImage = "url("+response.hdurl+")";
+    }
+  })
+}
