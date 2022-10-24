2881fa433f5d36c8732d4f19ff0b5d7d52f05d5d
bwhitt7 <bwhitt7@students.towson.edu>
2022-10-24 02:31:11 -0400

    bg

--- apod.js
+++ apod.js
@@ -1,19 +1,22 @@
 var url = "https://api.nasa.gov/planetary/apod?api_key=xEJ0WheX967HwigGbbLqsPjW6gRoZhfoGl4sEhNV";
 
 var req = new XMLHttpRequest();
+var response;
 
 req.open("GET", url);
 req.send();
 
 req.addEventListener("load", function(){
     if( req.status == 200 && req.readyState == 4){
-        var response = JSON.parse(req.responseText);
+        response = JSON.parse(req.responseText);
         document.getElementById("title").textContent = response.title;
         document.getElementById("date").textContent = response.date;
-        document.getElementById("pic").textContent = response.url;
-        document.getElementById("pic_hd").textContent = response.hdurl;
+        document.getElementById("picture").src = response.url;
+        document.getElementById("pic_hd").src = response.hdurl;
         document.getElementById("explanation").textContent = response.explanation;
-        //document.getElementById('content').style.backgroundImage = "url("+response.hdurl+")"
     }
 })
 
+window.onload = function(){
+    document.body.style.backgroundImage = "url("+response.url+")";
+}
