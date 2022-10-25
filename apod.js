b3598e033b11e855b4972985be14c4dc80ea6ab3
bwhitt7 <bwhitt7@students.towson.edu>
2022-10-25 10:33:55 -0400

    video handling?

--- apod.js
+++ apod.js
@@ -6,6 +6,7 @@ function findAPOD(date=""){
   var req = new XMLHttpRequest();
   var url = "https://api.nasa.gov/planetary/apod?api_key=";
   var api_key = "xEJ0WheX967HwigGbbLqsPjW6gRoZhfoGl4sEhNV";
+  var media_apod;
 
   req.open("GET", url + api_key +"&date=" +date, true);
   req.send();
@@ -16,6 +17,12 @@ function findAPOD(date=""){
       document.getElementById("title").textContent = response.title;
       document.getElementById("date").textContent = response.date;
       document.body.style.backgroundImage = "url("+response.hdurl+")";
+      if (response.media_type === "video"){
+        document.getElementById("vid").src = response.url+"&autoplay=1&controls=0&loop=1&disablekb=1&iv_load_policy=3&modestbranding=1";
+      }
+      else{
+        document.getElementById("vid").src = "";
+      }
     }
   }
 }
@@ -23,6 +30,5 @@ function findAPOD(date=""){
 
 function changeBG(){
   let inputVal = document.getElementById("apoddate").value;
-  // Displaying the value
   findAPOD(inputVal);
 }
