68083f3905100171a4615782a9f3466decf53ceb
bwhitt7 <bwhitt7@students.towson.edu>
2022-10-24 23:15:20 -0400

    big fix

--- apod.js
+++ apod.js
@@ -1,19 +1,28 @@
-window.onload = function(){
+$(document).ready(function() {
+  findAPOD();
+});
+
+function findAPOD(date=""){
   var req = new XMLHttpRequest();
   var url = "https://api.nasa.gov/planetary/apod?api_key=";
   var api_key = "xEJ0WheX967HwigGbbLqsPjW6gRoZhfoGl4sEhNV";
 
-  req.open("GET", url + api_key);
+  req.open("GET", url + api_key +"&date=" +date, true);
   req.send();
 
-  req.addEventListener("load", function(){
+  req.onreadystatechange =  function() {
     if(req.status == 200 && req.readyState == 4){
       var response = JSON.parse(req.responseText);
       document.getElementById("title").textContent = response.title;
       document.getElementById("date").textContent = response.date;
-      document.getElementById("pic").src = response.hdurl;
-      document.getElementById("explanation").textContent = response.explanation;
       document.body.style.backgroundImage = "url("+response.hdurl+")";
     }
-  })
+  }
+}
+
+
+function changeBG(){
+  let inputVal = document.getElementById("apoddate").value;
+  // Displaying the value
+  findAPOD(inputVal);
 }
